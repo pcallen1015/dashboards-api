bff41d157a2c5722fc99b1d7b8f1e46c1733b6d4
Philip Colin Allen <philip.allen@colorado.edu>
2019-10-28 15:42:39 -0600

    Updated default database host

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'development',
     port: 3000,
     db: {
-        host: process.env.MONGODB_HOST || 'sj-il-bmi-mongo',
+        host: process.env.MONGODB_HOST || 'localhost',
         port: process.env.MONGODB_PORT || 27017,
         name: process.env.MONGODB_NAME || 'bmi-sandbox'
     }
